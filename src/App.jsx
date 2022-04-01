b13bc13f3f366c1cbb509ff9ad37ae96f1ed7662
Nicholas-Paiva <nicholas.siqueira.paiva@gmail.com>
2022-04-01 19:38:06 -0300

    Adicionando as Habilidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React, {useState} from "react"
+import Habilidades from "./components/Habilidades"
 
 function App(){
 
-  const [name, setName] = useState("")
-  const [image, setImage] = useState("")
-  const [color, setColor] = useState("")
-  const [mesa, setMesa] = useState("")
-  //Idade, Nacionalidade, Ocupação, História
-  //Força, Agilidade, Percepcao, HabManuais, Intelecto, HabSociais
-  // Contatos
+  const [Nome, setNome] = useState("")
+  const [Imagem, setImagem] = useState("")
+  const [Cor, setCor] = useState("")
+  const [Mesa, setMesa] = useState("")
+  const [Idade, setIdade] = useState("")
+  const [Nacionalidade, setNacionalidade] = useState("")
+  const [Ocupacao, setOcupacao] = useState("")
+  const [Historia, setHistoria] = useState("")
+  const [Forca, setForca] = useState({})
+  const [Agilidade, setAgilidade] = useState({})
+  const [Percepcao, setPercepcao] = useState({})
+  const [Intelecto, setIntelecto] = useState({})
+  const [HabManuais, setHabManuais] = useState({})
+  const [HabSociais, setHabSociais] = useState({})
+  const [Contatos, setContatos] = useState([])
   
   const jsonData = {
-    name, image, color, mesa
+    Nome, Imagem, Cor, Mesa, Idade, Nacionalidade, Ocupacao, Historia, Forca, Agilidade, Percepcao, Intelecto, HabManuais, HabSociais, Contatos
    };
 
    function download(content, fileName, contentType) {
@@ -23,20 +32,55 @@ function App(){
    }
    
    function onDownload(){
-    download(JSON.stringify(jsonData), `${name}.json`, "text/plain");
+    download(JSON.stringify(jsonData), `${Nome}.json`, "text/plain");
    }
 
   return(
-    <div>
+    <div className="App">
       <form>
-        <label htmlFor="name">Nome do Personagem:</label><br/>
-        <input type="text" id="name" value={name} onChange={ (event) => setName(event.target.value)}/><br/>
-        <label htmlFor="image">Imagem:</label><br/>
-        <input type="text" id="image" value={image} onChange={ (event) => setImage(event.target.value)}/><br/>
-        <label htmlFor="color">Cor</label><br/>
-        <input type="text" id="color" value={color} onChange={ (event) => setColor(event.target.value)}/><br/>
-        <label htmlFor="mesa">Mesa</label><br/>
-        <input type="text" id="mesa" value={mesa} onChange={ (event) => setMesa(event.target.value)}/><br/>
+        <div className="Page1">
+          <label htmlFor="Nome">Nome do Personagem:</label><br/>
+          <input type="text" id="Nome" value={Nome} onChange={ (event) => setNome(event.target.value)}/><br/>
+          <label htmlFor="Imagem">Imagem:</label><br/>
+          <input type="text" id="Imagem" value={Imagem} onChange={ (event) => setImagem(event.target.value)}/><br/>
+          <label htmlFor="Cor">Cor</label><br/>
+          <input type="text" id="Cor" value={Cor} onChange={ (event) => setCor(event.target.value)}/><br/>
+          <label htmlFor="Mesa">Mesa</label><br/>
+          <input type="text" id="Mesa" value={Mesa} onChange={ (event) => setMesa(event.target.value)}/><br/>
+          <label htmlFor="Idade">Idade</label><br/>
+          <input type="text" id="Idade" value={Idade} onChange={ (event) => setIdade(event.target.value)}/><br/>
+          <label htmlFor="Nacionalidade">Nacionalidade</label><br/>
+          <input type="text" id="Nacionalidade" value={Nacionalidade} onChange={ (event) => setNacionalidade(event.target.value)}/><br/>
+          <label htmlFor="Ocupacao">Ocupacao</label><br/>
+          <input type="text" id="Ocupacao" value={Ocupacao} onChange={ (event) => setOcupacao(event.target.value)}/><br/>
+          <label htmlFor="Historia">História</label><br/>
+          <textarea type="text" id="Historia" value={Historia} onChange={ (event) => setHistoria(event.target.value)}/><br/>
+        </div>
+        <div className="ForcaDiv">
+          <label htmlFor="Forca">Forca</label><br/>
+          <Habilidades setHabilidade={setForca} name="Agilidade" />
+        </div>
+        <div className="AgilidadeDiv">
+          <label htmlFor="Agilidade">Agilidade</label><br/>
+          <Habilidades setHabilidade={setAgilidade} name="Agilidade" />
+        </div>
+        <div className="PercepcaoDiv">
+          <label htmlFor="Percepcao">Percepção</label><br/>
+          <Habilidades setHabilidade={setPercepcao} name="Percepcao" />
+        </div>
+        <div className="IntelectoDiv">
+          <label htmlFor="Intelecto">Intelecto</label><br/>
+          <Habilidades setHabilidade={setIntelecto} name="Intelecto" />
+        </div>
+        <div className="HabManuaisDiv">
+          <label htmlFor="HabManuais">Habilidades Manuais</label><br/>
+          <Habilidades setHabilidade={setHabManuais} name="HabManuais" />
+        </div>
+        <div className="HabSociaisDiv">
+          <label htmlFor="HabSociais">Habilidades Sociais</label><br/>
+          <Habilidades setHabilidade={setHabSociais} name="HabSociais" />
+        </div>
+
         <button onClick={onDownload}>Clica aqui puta</button>
       </form>
     </div>
